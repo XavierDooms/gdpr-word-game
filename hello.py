a30f2d09b6aff061e847bbf14b018e71c1628114
Xavier Dooms <dooms.xavier@live.be>
2018-07-22 12:58:58 +0200

    Fix default page

--- hello.py
+++ hello.py
@@ -4,19 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    html_string = "<html>\n"
-    html_string += "<head><style>body { font-family: Courier; }</style></head>\n"
-    html_string += "<body>\n"
-    html_string += "<h1>Find \"GDPR\"</h1>\n"
-    html_string += "<div>\n"
-    html_string += matrix_to_string(create_checked_matrix(10,30))
-    html_string += "</div>\n"
-    html_string += "<div hidden>\n"
-    html_string += "Location: {}\n".format(get_solution())
-    html_string += "</div>\n"
-    html_string += "\n</body>\n</html>"
-
-    return html_string
+    return word("GDPR")
 
 @app.route('/word/<word>')
 def word(word):

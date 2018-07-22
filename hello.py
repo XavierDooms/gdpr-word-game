3ac40fa062c5e68f8a5cec6c79ffbbff8ba21a23
Xavier Dooms <dooms.xavier@live.be>
2018-07-22 09:54:20 +0200

    Start of random word puzzle

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ def index():
     html_string += "<body>\n"
     html_string += "<h1>Find \"GDPR\"</h1>\n"
     html_string += "<div>\n"
-    html_string += matrix_to_string(create_checked_matrix(10,10))
+    html_string += matrix_to_string(create_checked_matrix(10,30))
     html_string += "</div>\n"
     html_string += "<div hidden>\n"
     html_string += "Location: {}\n".format(get_solution())
@@ -18,6 +18,18 @@ def index():
 
     return html_string
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/word/<word>')
+def word(word):
+    html_string = "<html>\n"
+    html_string += "<head><style>body { font-family: Courier; }</style></head>\n"
+    html_string += "<body>\n"
+    html_string += "<h1>Find \"{}\"</h1>\n".format(word)
+    html_string += "<div>\n"
+    html_string += matrix_to_string(create_checked_matrix(10,30))
+    html_string += "</div>\n"
+    html_string += "<div hidden>\n"
+    html_string += "Location: {}\n".format(get_solution())
+    html_string += "</div>\n"
+    html_string += "\n</body>\n</html>"
+
+    return html_string
